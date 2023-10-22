5bd20e187f0f8bbc6f702f1a5304b6e441c0364d
jonorgan <jonorgan26@gmail.com>
2023-10-22 17:56:22 -0400

    Almost done with regression

--- regression/hw6_reg.py
+++ regression/hw6_reg.py
@@ -2,15 +2,62 @@
 ### CS667 Data Science with Python, Homework 6, Jon Organ
 ###
 
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 import pandas as pd
-# From year 1 of your stock, try to find the y = mx + b (looking for m and b values) of your stock
-# Assign them randomly at first, then evaluate, take initial score, alter m and b values, re-evaluate
-# if score improved, move farther in that direction, if not go in opposite direction, steps should be 
-# proportional to change in the score
-	# Start by adding to both m and b, if that keeps working keep going
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn import preprocessing
 
-# Evaluate process: Calculate the y value for the x (average return) input, if its greater than the
-# observed y (volatility) value, then assign it to the green group, otherwise red group
-	# green and red might have to be flipped here
-# Check group assignments against actual groups, this is the resulting score
-	# score = correct / total
+file = pd.read_csv("cmg_weeks.csv")
+
+returns = file["Avg_Return"][file['Week'] <= 50].to_numpy().reshape(-1, 1)
+color = file["Color"][file['Week'] <= 50].to_numpy()
+
+returns2 = file["Avg_Return"][(file['Week'] > 50) & (file['Week'] <= 100)].to_numpy().reshape(-1, 1)
+color2 = file["Color"][(file['Week'] > 50) & (file['Week'] <= 100)].to_numpy()
+
+
+model = LogisticRegression(solver='liblinear', random_state=0)
+model.fit(returns, color)
+
+
+# Question 1 ==========================================================================================
+print("Question 1:")
+print("The logistic regression classifier found the following coefficients:")
+print("Slope:", model.coef_)
+print("Intercept:", model.intercept_)
+
+
+
+
+print("\n")
+# Question 2 ==========================================================================================
+print("Question 2:")
+print("Using year 1 as training data, the accuracy for year 2 is " + str(model.score(returns2, color2)))
+
+
+
+
+print("\n")
+# Question 3 ==========================================================================================
+print("Question 3:")
+print(confusion_matrix(color2, model.predict(returns2)))
+
+
+
+print("\n")
+# Question 4 ==========================================================================================
+print("Question 4:")
+print("The True Positive Rate is 1, the True Negative Rate is 0")
+
+
+
+print("\n")
+# Question 5 ==========================================================================================
+print("Question 5:")
+
+
+
